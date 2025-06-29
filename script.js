c6498f261e02329b86994a6a482b77d233a3106a
Marckzito404 <lukenekki@gmail.com>
2025-06-28 21:45:02 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,30 @@
-document.addEventListener('DOMContentLoaded', () => {
+const navLinks = document.querySelectorAll('nav a');
+const sections = document.querySelectorAll('.content-section');
 
-    const navLinks = document.querySelectorAll('nav a');
-    const sections = document.querySelectorAll('.content-section');
-
-    // Função que mostra a página certa e esconde as outras
-    function showPage(pageId) {
-        sections.forEach(section => {
-            section.classList.remove('active');
-        });
-
-        const pageToShow = document.getElementById(pageId);
-        if (pageToShow) {
-            pageToShow.classList.add('active');
-        }
-    }
-    navLinks.forEach(link => {
-        link.addEventListener('click', (event) => {
-            // Isso previne que a página recarregue
-            event.preventDefault(); 
-            
-            const targetPage = link.getAttribute('data-page');
-
-            // Chama nossa função pra fazer a troca
-            showPage(targetPage);
-
-            // Atualiza o visual do menu pra destacar o link ativo
-            navLinks.forEach(l => l.classList.remove('active'));
-            link.classList.add('active');
-        });
+// Função que mostra a página certa e esconde as outras .
+function showPage(pageId) {
+    sections.forEach(section => {
+        section.classList.remove('active');
     });
 
+    const pageToShow = document.getElementById(pageId);
+    if (pageToShow) {
+        pageToShow.classList.add('active');
+    }
+}
+
+navLinks.forEach(link => {
+    link.addEventListener('click', (event) => {
+        // previne que a página recarregue
+        event.preventDefault(); 
+        
+        const targetPage = link.getAttribute('data-page');
+
+        // Chama a função pra fazer a troca
+        showPage(targetPage);
+
+        // Atualiza o visual do menu pra destacar o link ativo
+        navLinks.forEach(l => l.classList.remove('active'));
+        link.classList.add('active');
+    });
 });
